27cc76627fbe992f988cbec3fd2ebb23b4a36b6c
avimallik <armavicse@gmail.com>
2025-05-21 20:00:18 +0600

    Comparisons of average data quality of survival incentive mechanisms

---
+++ comparision_avg_data_quality_survival_incentive.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+labels = ['FRI', 'QAI', 'SII (Proposed)']
+avg_quality = [0.72, 0.85, 0.78]
+avg_survival = [0.55, 0.50, 0.75]
+avg_swdi = [0.22, 0.21, 0.44]
+
+x = np.arange(len(labels))
+width = 0.25
+
+fig, ax = plt.subplots(figsize=(10,6))
+rects1 = ax.bar(x - width, avg_quality, width, label='Average Data Quality')
+rects2 = ax.bar(x, avg_survival, width, label='Average Survival')
+rects3 = ax.bar(x + width, avg_swdi, width, label='Average SWDI')
+
+ax.set_xlabel('Incentive Mechanism')
+ax.set_title('Average Data Quality, Survival, and SWDI Comparison')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+ax.grid(axis='y')
+
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate(f'{height:.2f}',
+                    xy=(rect.get_x() + rect.get_width()/2, height),
+                    xytext=(0,3),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+
+plt.tight_layout()
+plt.show()
